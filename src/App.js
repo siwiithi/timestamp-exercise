6da5501af99348f3607f36cb8dccf405d74d96ae
SIWIITHI <siwiphorn.thi@gmail.com>
2018-10-13 18:01:59 +0700

    Console log result

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 import moment from 'moment'
-
+import _ from 'lodash'
+// date 10/10/2018
+// date 10/10/2018
+// xxxx
+// xxxx
+// date 10/11/2018
+// xxx
+// date 10/12/2018
+// xxx
+// .
+// .
+// .
+// date xx/xx/2018
+// xxxx
+// วันเดียวกันไม่ต้อง show date ซ้ำ
 class App extends Component {
   changeDateFormat() {
     const data = [
@@ -16,14 +30,29 @@ class App extends Component {
       {timestamp:1536939324, order:'banana'},
       {timestamp:1536939324, order:'apple'},
     ]
-    const changeData = data.map((value) => {
-     return(
-      <div> 
-        <div>{moment(value.timestamp).format('L')}</div>
-        <div>{value.order}</div>
-      </div>
-    )})
-    return changeData
+    const newDate = data
+      .map((value, key)=>  {
+        return(
+          {
+            date: moment.unix(value.timestamp).format("MM/DD/YYYY"),
+            order: value.order
+            
+          }
+        )
+      })
+
+    const filterData = _.groupBy(newDate, (value) => (value.date))
+    const convertedData = Object.keys(filterData).map(function(key) {
+      return [key, filterData[key]]
+    })  
+    // console.log('convert', convertedData)
+    let test1 = convertedData.forEach(function(element){
+      console.log('date', element[0])
+      element[1].forEach(function(testData){
+        console.log('order', testData.order)
+      })
+    })
+    
   }
 
   render() {
